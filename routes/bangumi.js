5765d371a706e09ead000b62802ef6017a98b9f6
DIYgod <diy.d.god@gmail.com>
2017-10-17 00:57:34 +0800

    bangumi rss

--- routes/bangumi.js
+++ routes/bangumi.js
@@ -1,5 +1,5 @@
-var fetch = require('node-fetch');
 var logger = require('../tools/logger');
+var request = require('request');
 
 module.exports = function (req, res) {
     res.header('Content-Type', 'application/xml; charset=utf-8');
@@ -11,40 +11,54 @@ module.exports = function (req, res) {
 
     var seasonid = req.params.seasonid;
 
-    logger.info(`bilibili2RSS seasonid ${seasonid} form origin, IP: ${ip}`);
+    logger.info(`bilibili2RSS seasonid ${seasonid}, IP: ${ip}`);
 
-    // fetch(`https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${uid}`, {
-    //     headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'},
-    // }).then(
-    //     response => response.json()
-    // ).then((data) => {
-    //     const list = data.data.vlist;
-    //     var rss =
-    //     `<?xml version="1.0" encoding="UTF-8"?>
-    //     <rss version="2.0">
-    //     <channel>
-    //     <title>${uid}的bilibili稿件</title>
-    //     <link>https://space.bilibili.com/${uid}</link>
-    //     <description>${uid}的bilibili稿件，使用 bilibili2RSS(https://github.com/DIYgod/bilibili2RSS) 构建</description>
-    //     <language>zh-cn</language>
-    //     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    //     <ttl>300</ttl>`
-    //                 for (var i = 0; i < list.length; i++) {
-    //                     rss +=`
-    //     <item>
-    //         <title><![CDATA[${list[i].title}]]></title>
-    //         <description><![CDATA[${list[i].description}<br><img src="${list[i].pic}">]]></description>
-    //         <pubDate>${new Date(list[i].created * 1000).toUTCString()}</pubDate>
-    //         <guid>https://www.bilibili.com/video/av${list[i].aid}</guid>
-    //         <link>https://www.bilibili.com/video/av${list[i].aid}</link>
-    //     </item>`
-    //                 }
-    //                 rss += `
-    //     </channel>
-    //     </rss>`
-    //                 res.send(rss);
-    //     }
-    // ).catch(
-    //     e => logger.error("bilibili2RSS Error: getting widget", e)
-    // );
+    request.get({
+        url: `https://bangumi.bilibili.com/jsonp/seasoninfo/${seasonid}.ver?callback=seasonListCallback&jsonp=jsonp&_=${+new Date()}`,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
+            'Referer': `https://bangumi.bilibili.com/anime/${seasonid}/`,
+            'Origin': 'https://bangumi.bilibili.com'
+        }
+    }, function (err, httpResponse, body) {
+        var data;
+        try {
+            data = JSON.parse(body.match(/^seasonListCallback\((.*)\);$/)[1]);
+        }
+        catch(e) {
+            data = {
+                result: {
+                    episodes: []
+                }
+            }
+        }
+        var result = data.result || {
+            episodes: []
+        };
+        var list = result.episodes;
+        var rss =
+            `<?xml version="1.0" encoding="UTF-8"?>
+        <rss version="2.0">
+        <channel>
+        <title>${result.title}</title>
+        <link>https://bangumi.bilibili.com/anime/${seasonid}/</link>
+        <description>${result.evaluate} - 使用 bilibili2RSS(https://github.com/DIYgod/bilibili2RSS) 构建</description>
+        <language>zh-cn</language>
+        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+        <ttl>300</ttl>`
+        for (var i = 0; i < list.length; i++) {
+            rss += `
+        <item>
+            <title><![CDATA[第${list[i].index}话 ${list[i].index_title}]]></title>
+            <description><![CDATA[更新时间：${list[i].update_time}<img src="${list[i].cover}">]]></description>
+            <pubDate>${new Date(list[i].update_time).toUTCString()}</pubDate>
+            <guid>${list[i].webplay_url}</guid>
+            <link>${list[i].webplay_url}</link>
+        </item>`
+        }
+        rss += `
+        </channel>
+        </rss>`
+        res.send(rss);
+    });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
-var fetch = require('node-fetch');
 var logger = require('../tools/logger');
+var redis = require('../tools/redis');
+var request = require('request');
 
 module.exports = function (req, res) {
     res.header('Content-Type', 'application/xml; charset=utf-8');
@@ -8,43 +9,74 @@ module.exports = function (req, res) {
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
+    var name;
 
     var uid = req.params.uid;
 
-    logger.info(`bilibili2RSS uid ${uid} form origin, IP: ${ip}`);
-
-    fetch(`https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${uid}`, {
-        headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'},
-    }).then(
-        response => response.json()
-    ).then((data) => {
-        const list = data.data.vlist;
-        var rss =
-        `<?xml version="1.0" encoding="UTF-8"?>
-        <rss version="2.0">
-        <channel>
-        <title>${uid}的bilibili稿件</title>
-        <link>https://space.bilibili.com/${uid}</link>
-        <description>${uid}的bilibili稿件，使用 bilibili2RSS(https://github.com/DIYgod/bilibili2RSS) 构建</description>
-        <language>zh-cn</language>
-        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-        <ttl>300</ttl>`
-                    for (var i = 0; i < list.length; i++) {
-                        rss +=`
-        <item>
-            <title><![CDATA[${list[i].title}]]></title>
-            <description><![CDATA[${list[i].description}<br><img src="${list[i].pic}">]]></description>
-            <pubDate>${new Date(list[i].created * 1000).toUTCString()}</pubDate>
-            <guid>https://www.bilibili.com/video/av${list[i].aid}</guid>
-            <link>https://www.bilibili.com/video/av${list[i].aid}</link>
-        </item>`
-                    }
-                    rss += `
-        </channel>
-        </rss>`
-                    res.send(rss);
+    redis.client.get(`bilibiliuid${uid}`, function(err, reply) {
+        if (reply) {
+            logger.info(`bilibiliuid${uid} form redis, IP: ${ip}`);
+            name = reply;
+            getVideos();
         }
-    ).catch(
-        e => logger.error("bilibili2RSS Error: getting widget", e)
-    );
+        else {
+            logger.info(`bilibiliuid${uid} form origin, IP: ${ip}`);
+            request.post({
+                url: 'https://space.bilibili.com/ajax/member/GetInfo',
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
+                    'Referer': `https://space.bilibili.com/${uid}/`,
+                    'Origin': 'https://space.bilibili.com'
+                },
+                form: {
+                    mid: uid
+                }
+            }, function (err, httpResponse, body) {
+                console.log(JSON.parse(body));
+                name = JSON.parse(body).data.name;
+                redis.set(`bilibiliuid${uid}`, name);
+                getVideos();
+            });
+        }
+    });
+
+    function getVideos () {
+        logger.info(`bilibili2RSS uid ${uid}, IP: ${ip}`);
+
+        request.get({
+            url: `https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${uid}`,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',
+                'Referer': `https://space.bilibili.com/${uid}/`,
+                'Origin': 'https://space.bilibili.com'
+            }
+        }, function (err, httpResponse, body) {
+            const data = JSON.parse(body);
+            const list = data.data.vlist;
+            var rss =
+                `<?xml version="1.0" encoding="UTF-8"?>
+            <rss version="2.0">
+            <channel>
+            <title>${name}的bilibili空间</title>
+            <link>https://space.bilibili.com/${uid}</link>
+            <description>${name}的bilibili空间，使用 bilibili2RSS(https://github.com/DIYgod/bilibili2RSS) 构建</description>
+            <language>zh-cn</language>
+            <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+            <ttl>300</ttl>`
+            for (var i = 0; i < list.length; i++) {
+                rss += `
+            <item>
+                <title><![CDATA[${list[i].title}]]></title>
+                <description><![CDATA[${list[i].description}<br><img src="${list[i].pic}">]]></description>
+                <pubDate>${new Date(list[i].created * 1000).toUTCString()}</pubDate>
+                <guid>https://www.bilibili.com/video/av${list[i].aid}</guid>
+                <link>https://www.bilibili.com/video/av${list[i].aid}</link>
+            </item>`
+            }
+            rss += `
+            </channel>
+            </rss>`
+            res.send(rss);
+        });
+    }
 };
